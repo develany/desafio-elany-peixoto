d5010dd51fc2f8c260e0cb78527bfa391339f218
Elany Peixoto <elanysantos5@outlook.com>
2023-08-11 13:51:36 -0300

    solução do desafio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -4,35 +4,50 @@ class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
 
-        if (itens.lenght === 0) {
+        const itensExtrasRelacionados = {
+            'queijo': 'sanduiche',
+            'chantily': 'cafe'
+        };
+        let valorTotal = 0
+
+        if (itens.length === 0) {
             return "Não há itens no carrinho de compra!"
         }
 
-        let valorTotal = 0
-
-        for (itemInfo of itens) {
+        for (const itemInfo of itens) {
             const [codigo, quantidade] = itemInfo.split(',');
             const item = Cardapio.find(item => item.codigo === codigo);
             if (!item) {
                 return "Item inválido!";
-
             }
 
+            if (!parseInt(quantidade)) {
+                return "Quantidade inválida!";
+            }
+
+            if (itemInfo.includes(',')) {
+                const [itemCodigo, _] = itemInfo.split(',');
+
+                if (itensExtrasRelacionados[itemCodigo]) {
+                    const itemPrincipal = itensExtrasRelacionados[itemCodigo];
+                    if (!itens.some(item => item.split(',')[0] === itemPrincipal)) {
+                        return `Item extra não pode ser pedido sem o principal`;
+                    }
+                }
+            }
             valorTotal += item.valor * parseInt(quantidade);
-
-            if (itemInfo.includes(',') && !itens.includes(item.codigo)) {
-                return "Item extra não pode ser pedido sem o principal";
-            }
-
         }
-        if (metodoDePagamento === 'dinheiro') {
+        if (metodoDePagamento === 'debito') {
+            valorTotal = valorTotal
+        } else if (metodoDePagamento === 'dinheiro') {
             valorTotal *= 0.95; // 5% de desconto
         } else if (metodoDePagamento === 'credito') {
             valorTotal *= 1.03; // 3% de acréscimo
+        } else {
+            return "Forma de pagamento inválida!"
         }
 
-        // Formatar o valor total e retornar
-        const valorTotalFormatado = `R$ ${valorTotal.toFixed(2)}`;
+        const valorTotalFormatado = `R$ ${valorTotal.toFixed(2).replace(".", ',')}`;
         return valorTotalFormatado;
     }
 
